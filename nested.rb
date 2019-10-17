2f5df9aefc913a07e734bef026fc5fe7176c2cdd
Jen Lindner <jenlindner22@gmail.com>
2019-10-17 19:46:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,6 +89,7 @@ def adding_matz
      programmer_hash.delete(:dennis_ritchie)
 
     programmer_hash[:yukihiro_matsumoto] = {}
+    
     [:known_for] = "Ruby"
     programmer_hash[:yukihiro_matsumoto][:languages] ="LISP", "C"
 
