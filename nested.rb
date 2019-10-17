f5741d885778f9c33091d3b2ed215a69d3d0f901
Jen Lindner <jenlindner22@gmail.com>
2019-10-17 19:54:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,6 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Smalltalk", "LISP", "Assembly"
+programmer_hash[:dennis_ritchie][:languages] = "Smalltalk", "Assembly" "LISP"
 programmer_hash
 end
